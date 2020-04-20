5dd20499ba08eeb137e209748eee98af1355126a
matsbyfl <mats@byfuglien.net>
2020-04-20 15:43:24 +0200

    Use memoryStore node module to handle sessions. This will not leak memory as the built in memoryStore meant for development

--- server/src/session.js
+++ server/src/session.js
@@ -1,16 +1,17 @@
 const { host } = require('./config')
 const session = require('express-session')
-
-const SESSION_MAX_AGE_MILLISECONDS = 24 * 60 * 60 * 1000
+const MemoryStore = require('memorystore')(session)
+const TWENTY_FOUR_HOURS = 24 * 60 * 60 * 1000
 
 const setup = app => {
   app.set('trust proxy', 1)
   app.use(
     session({
       cookie: {
-        maxAge: SESSION_MAX_AGE_MILLISECONDS,
+        maxAge: TWENTY_FOUR_HOURS,
         sameSite: 'lax'
       },
+      store: new MemoryStore({ checkPeriod: TWENTY_FOUR_HOURS }),
       secret: host.sessionKey,
       name: host.cookieName,
       resave: false,
