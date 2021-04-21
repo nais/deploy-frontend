4684887f3171db89fc03e3b8bda2375ee651f2c5
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-04-21 15:46:39 +0200

    Refactor, split into files

--- frontend/js/dashboard/dashboard.tsx
+++ frontend/js/dashboard/dashboard.tsx
@@ -1,166 +1,15 @@
 'use strict';
 import React, { useEffect, useReducer } from 'react'
-import Deployment from './deployment'
 import { logPageView } from '../amplitude.js'
-import { Close } from '@navikt/ds-icons'
 import { getDeployments, DeploymentList } from './deploymentAPI'
-import NavFrontendSpinner from "nav-frontend-spinner";
-
-const FilterPanel = ({ dashboardState, dispatch }) => {
-  const emptyListStyle: React.CSSProperties = {
-    fontStyle: 'italic',
-    display: 'inline-block',
-    marginRight: '.5em',
-    fontSize: '75%',
-  }
-  const buttonStyle: React.CSSProperties = {
-    display: 'inline-block',
-    background: '#eee',
-    fontFamily: "'Source Sans Pro', Arial, sans-serif",
-    width: 'fit-content',
-    padding: '.25em .4em',
-    fontSize: '75%',
-    borderRadius: '10rem',
-    lineHeight: '1',
-    border: '1px solid #ddd',
-    cursor: 'pointer',
-  }
-  return !dashboardState.filters.size ? (
-    <div style={emptyListStyle}>No filters currently enabled. Click a team name to filter.</div>
-  ) : (
-    <div>
-      <div style={emptyListStyle}>Filters:</div>
-      {Array.from(dashboardState.filters.entries()).map(([key, value]) => (
-        <div
-          style={buttonStyle}
-          key={`${key}:${value}`}
-          onClick={() => dispatch({ type: 'FILTER_REMOVE', value: value })}
-        >
-          <Close style={{ fontSize: '.7rem', marginRight: '.5em' }} />
-          <span>
-            {key}: {value}
-          </span>
-        </div>
-      ))}
-    </div>
-  )
-}
-
-const DeploymentsTable = ({ dashboardState, dispatch }) => {
-  const deploymentFilter = (deployment, filters) => {
-    for (let [key, value] of filters) {
-      if (key == 'team') {
-        if (deployment.deployment.team != value) {
-          return false
-        }
-      }
-    }
-    return true
-  }
-
-  const loadingSpinner = () => (
-      <tr>
-        <td colSpan={6} style={{textAlign: 'center'}}>
-          <NavFrontendSpinner/>
-        </td>
-      </tr>
-  )
-
-  return (
-    <div>
-      <table className="tabell">
-        <thead>
-          <tr>
-            <th>Resource(s)</th>
-            <th>Created</th>
-            <th>Team</th>
-            <th>Cluster</th>
-            <th>Status</th>
-            <th>Links</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dashboardState.deployments
-            .filter((x) => deploymentFilter(x, dashboardState.filters))
-            .map((x) => (
-              <Deployment key={x.deployment.id} dispatch={dispatch} deployment={x} />
-            ))}
-          {dashboardState.loading ? loadingSpinner() : null}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+import {deploymentReducer, initialState} from "./deploymentReducer";
+import DeploymentTable from "./deploymentFilteredTable";
+import FilterPanel from "./filterPanel";
 
 export default function Dashboard(props) {
   logPageView('/')
 
-  type DeploymentFilterChange =
-    | { type: 'FILTER_ADD'; value: string }
-    | { type: 'FILTER_REMOVE'; value: string }
-    | { type: 'FILTER_CLEAR' }
-
-  type FetchResult =
-    | { type: 'FETCH_SUCCESS'; payload: object }
-    | { type: 'FETCH_ERROR'; error: JSX.Element }
-
-  interface dashboardState {
-    loading: boolean
-    deployments: DeploymentList
-    filters: Map<string, string>
-    error: JSX.Element | null
-  }
-
-  const initialState: dashboardState = {
-    loading: true,
-    deployments: new Array(),
-    filters: new Map(),
-    error: null,
-  }
-
-  const deploymentsReducer = (
-    state: dashboardState,
-    action: DeploymentFilterChange | FetchResult
-  ) => {
-    switch (action.type) {
-      case 'FETCH_SUCCESS':
-        return {
-          loading: false,
-          deployments: action.payload,
-          filters: state.filters,
-          error: null,
-        }
-      case 'FETCH_ERROR':
-        return {
-          loading: false,
-          deployments: state.deployments,
-          filters: state.filters,
-          error: action.error,
-        }
-      case 'FILTER_REMOVE':
-        const removeFilters = new Map(state.filters)
-        removeFilters.delete('team')
-        return {
-          loading: true,
-          deployments: state.deployments,
-          filters: removeFilters,
-          error: state.error,
-        }
-      case 'FILTER_ADD':
-        const addFilters = new Map(state.filters)
-        addFilters.set('team', action.value)
-        return {
-          loading: true,
-          deployments: state.deployments,
-          filters: addFilters,
-          error: state.error,
-        }
-      default:
-        return state
-    }
-  }
-
-  const [dashboardState, deploymentsDispatch] = useReducer(deploymentsReducer, initialState)
+  const [dashboardState, deploymentsDispatch] = useReducer(deploymentReducer, initialState)
 
   useEffect(() => {
     getDeployments(dashboardState.filters)
@@ -186,7 +35,7 @@ export default function Dashboard(props) {
   return (
     <div className={'mainContent'}>
       <FilterPanel dashboardState={dashboardState} dispatch={deploymentsDispatch} />
-      <DeploymentsTable dashboardState={dashboardState} dispatch={deploymentsDispatch} />
+      <DeploymentTable dashboardState={dashboardState} dispatch={deploymentsDispatch} />
     </div>
   )
 }
---
+++ frontend/js/dashboard/deploymentReducer.ts
@@ -0,0 +1,66 @@
+import {DeploymentList} from "./deploymentAPI";
+
+type DeploymentFilterChange =
+    | { type: 'FILTER_ADD'; value: string }
+    | { type: 'FILTER_REMOVE'; value: string }
+    | { type: 'FILTER_CLEAR' }
+
+type FetchResult =
+    | { type: 'FETCH_SUCCESS'; payload: object }
+    | { type: 'FETCH_ERROR'; error: JSX.Element }
+
+export interface dashboardState {
+    loading: boolean
+    deployments: DeploymentList
+    filters: Map<string, string>
+    error: JSX.Element | null
+}
+
+export const initialState: dashboardState = {
+    loading: true,
+    deployments: new Array(),
+    filters: new Map(),
+    error: null,
+}
+
+export const deploymentReducer = (
+    state: dashboardState,
+    action: DeploymentFilterChange | FetchResult
+) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS':
+            return {
+                loading: false,
+                deployments: action.payload,
+                filters: state.filters,
+                error: null,
+            }
+        case 'FETCH_ERROR':
+            return {
+                loading: false,
+                deployments: state.deployments,
+                filters: state.filters,
+                error: action.error,
+            }
+        case 'FILTER_REMOVE':
+            const removeFilters = new Map(state.filters)
+            removeFilters.delete('team')
+            return {
+                loading: true,
+                deployments: state.deployments,
+                filters: removeFilters,
+                error: state.error,
+            }
+        case 'FILTER_ADD':
+            const addFilters = new Map(state.filters)
+            addFilters.set('team', action.value)
+            return {
+                loading: true,
+                deployments: state.deployments,
+                filters: addFilters,
+                error: state.error,
+            }
+        default:
+            return state
+    }
+}
