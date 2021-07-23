2af976c6f69831c72ed10510873003e36c1fa69c
Sondre Nilsen <sondre.nilsen@nav.no>
2021-07-23 13:48:29 +0200

    Remove Babel, use json-server for local development and mocking

---
+++ packages/frontend/server.js
@@ -0,0 +1,46 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.post('/downstream/api/v1/apikey/:team', (req, res) => {
+  setTimeout(() => res.statu(201).send(), 1000)
+  // 'This is a hardcoded error (in apiKeyMock.js). Do not panic. We only use this for testing how error handling works in the frontend. And to make sure we can see this error message.'
+  // res.status(500).send()
+})
+
+server.post('/api/v1/apikey/:team', (req, res) => {
+  setTimeout(() => res.statu(201).send(), 1000)
+  // 'This is a hardcoded error (in apiKeyMock.js). Do not panic. We only use this for testing how error handling works in the frontend. And to make sure we can see this error message.'
+  // res.status(500).send()
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+server.use(
+  jsonServer.rewriter({
+    '/api/v1/apikey': '/apikeys',
+    '/downstream/api/v1/apikey/': '/apikeys',
+    '/downstream/api/v1/dashboard/deployments': '/deployments',
+    '/downstream/api/v1/dashboard/deployments?': '/deployments',
+  })
+)
+
+// Use default router
+server.use(router)
+server.listen(4000, () => {
+  console.log('JSON Server is running')
+})
--- packages/server/test/style-mock.js
+++
@@ -1,2 +0,0 @@
-// Empty module that we use as a mock for css, images and other resources when running tests of react components
-module.exports = {}
