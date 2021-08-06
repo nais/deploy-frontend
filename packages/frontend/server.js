52c7415af4c86b08cf99335af1326c72651d894c
Sondre Nilsen <sondre.nilsen@nav.no>
2021-08-06 15:57:00 +0200

    Fix mock JSON API for filtering responses

--- packages/frontend/server.js
+++ packages/frontend/server.js
@@ -3,21 +3,14 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+const data = require('./db.json')
+
+const filterComponent = (path) => {
+  return path.split('=')[1].replace('&', '').trim()
+}
+
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
-
-// Add custom routes before JSON Server router
-server.post('/downstream/api/v1/apikey/:team', (req, res) => {
-  setTimeout(() => res.status(201).send(), 1000)
-  // 'This is a hardcoded error (in apiKeyMock.js). Do not panic. We only use this for testing how error handling works in the frontend. And to make sure we can see this error message.'
-  // res.status(500).send()
-})
-
-server.post('/api/v1/apikey/:team', (req, res) => {
-  setTimeout(() => res.status(201).send(), 1000)
-  // 'This is a hardcoded error (in apiKeyMock.js). Do not panic. We only use this for testing how error handling works in the frontend. And to make sure we can see this error message.'
-  // res.status(500).send()
-})
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
@@ -34,12 +27,34 @@ server.use(
   jsonServer.rewriter({
     '/api/v1/apikey': '/apikeys',
     '/downstream/api/v1/apikey/': '/apikeys',
-    '/api/v1/dashboard/deployments': '/deployments',
     '/api/v1/dashboard/deployments?': '/deployments',
-    '/downstream/api/v1/dashboard/deployments': '/deployments',
     '/downstream/api/v1/dashboard/deployments?': '/deployments',
+    '/downstream/api/v1/dashboard/deployments?*': '/deployments/$1',
   })
 )
+
+// Add custom routes before JSON Server router
+server.post('/downstream/api/v1/apikey/:team', (req, res) => {
+  setTimeout(() => res.status(201).send(), 1000)
+  // 'This is a hardcoded error (in apiKeyMock.js). Do not panic. We only use this for testing how error handling works in the frontend. And to make sure we can see this error message.'
+  // res.status(500).send()
+})
+
+server.post('/api/v1/apikey/:team', (req, res) => {
+  setTimeout(() => res.status(201).send(), 1000)
+  // 'This is a hardcoded error (in apiKeyMock.js). Do not panic. We only use this for testing how error handling works in the frontend. And to make sure we can see this error message.'
+  // res.status(500).send()
+})
+
+server.get('/deployments/:team', (req, res) => {
+  const {
+    params: { team },
+  } = req
+  const pretty = filterComponent(team)
+  const filtered = data.deployments.deployments.filter((d) => d.deployment.team === pretty)
+
+  res.status(200).send({ deployments: filtered })
+})
 
 // Use default router
 server.use(router)
