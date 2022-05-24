c7a45f113d1a9360677174bf986d561d1b75e95c
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2022-05-24 12:03:24 +0200

    undo refactor -> smaller diff

--- packages/server/src/reverse-proxy.js
+++ packages/server/src/reverse-proxy.js
@@ -2,32 +2,30 @@ const authUtils = require('./auth/auth-utils')
 const { host, proxyConfig, logger } = require('./config')
 const proxy = require('express-http-proxy')
 
-const proxyReqOptDecorator = (options, req) => {
-  if (host.authenticationEnabled) {
-    if (host.authProviderAzure) {
-      return new Promise((resolve, reject) => {
-        authUtils.getOnBehalfOfAccessToken(authClient, req, api).then(
-          (access_token) => {
-            options.headers.Authorization = `Bearer ${access_token}`
-            resolve(options)
-          },
-          (error) => reject(error)
-        )
-      })
-    } else if (host.authProviderGoogle) {
-      return new Promise((resolve, reject) => {
-        options.headers.XApiKey = proxyConfig.apiKey
-        resolve(options)
-      })
-    }
-  } else {
-    return new Promise((resolve, reject) => resolve(options))
-  }
-}
-
 const options = (api, authClient) => ({
   parseReqBody: false,
-  proxyReqOptDecorator,
+  proxyReqOptDecorator: (options, req) => {
+    if (host.authenticationEnabled) {
+      if (host.authProviderAzure) {
+        return new Promise((resolve, reject) => {
+          authUtils.getOnBehalfOfAccessToken(authClient, req, api).then(
+            (access_token) => {
+              options.headers.Authorization = `Bearer ${access_token}`
+              resolve(options)
+            },
+            (error) => reject(error)
+          )
+        })
+      } else if (host.authProviderGoogle) {
+        return new Promise((resolve, reject) => {
+          options.headers.XApiKey = proxyConfig.apiKey
+          resolve(options)
+        })
+      }
+    } else {
+      return new Promise((resolve, reject) => resolve(options))
+    }
+  },
   proxyReqPathResolver: (req) => {
     const newPath = req.originalUrl.replace(api.path, '').replace('//', '/')
     logger.debug(`Proxying request from '${req.originalUrl}' to '${newPath}'`)
